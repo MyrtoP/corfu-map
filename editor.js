dc49d41403617594633afa896e9f6df6fba3641c
Myrto <32554413+MyrtoP@users.noreply.github.com>
2018-02-16 16:29:08 +0200

    Update editor.js

--- editor.js
+++ editor.js
@@ -8,7 +8,9 @@ function init() {
   // Initialise the map.
   map = new google.maps.Map(document.getElementById('map-holder'), {
     center: {lat: 39.6249838, lng: 19.922346100000027},
-    zoom: 3,
+    zoom: 13
+    center: locationRio,
+    gestureHandling: 'cooperative'
     mapTypeControl: false,
     streetViewControl: false,
     fullscreenControl: false,
