ca1480dc6f66eee5b15b68dca67d9fe87a110668
Myrto <32554413+MyrtoP@users.noreply.github.com>
2018-03-12 00:52:57 +0200

    Add files via upload

--- editor.js
+++ editor.js
@@ -5,20 +5,15 @@ var geoJsonOutput;
 var downloadLink;
 
 function init() {
-  // Initialise the map.zoom set to 14 and added menu slide button
+  // Initialise the map.
   map = new google.maps.Map(document.getElementById('map-holder'), {
     center: {lat: 39.6249838, lng: 19.922346100000027},
-    zoom: 14,  
+    zoom: 14,
     mapTypeControl: false,
     streetViewControl: false,
-    fullscreenControl: false, 
-    mapTypeControl: true,    //enable map option control
-    mapTypeControlOptions: {
-       style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,   //add dropdown menu for the map style
-       position: google.maps.ControlPosition.TOP_LEFT,       //menu on top center of the map
-       mapTypeIds: ['satellite','roadmap']
+    fullscreenControl: false,
+    mapTypeId: 'satellite'
   });
-	  
 	
   map.data.setControls(['Point', 'LineString', 'Polygon']);
   map.data.setStyle({
@@ -27,16 +22,15 @@ function init() {
     clickable: true
   });
 
-	// DINATOTITA DIAGRAFIS TOU MONOPATIOU 3 erotima
-/*https://developers.google.com/maps/documentation/javascript/examples/delete-vertex-menu 
-https://gist.github.com/zross/9893056*/
   bindDataLayerListeners(map.data);
 
   // Retrieve HTML elements.
   var mapContainer = document.getElementById('map-holder');
   geoJsonOutput = document.getElementById('geojson-output');
   downloadLink = document.getElementById('download-link');
+  
 }
+
 
 google.maps.event.addDomListener(window, 'load', init);
 
